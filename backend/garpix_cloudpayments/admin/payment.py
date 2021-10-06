4322f7013b6e933144282890748c3e43dd37c183
marychev <marychevmihail3737@gmail.com>
2021-10-06 16:30:20 +0300

    Fixed html form

--- backend/garpix_cloudpayments/admin/payment.py
+++ backend/garpix_cloudpayments/admin/payment.py
@@ -4,6 +4,7 @@ from ..models.payment import Payment
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('order_number', 'created_at', 'price', 'status', 'is_test')
-    list_filter = ('is_test', 'status')
-    search_fields = ('order_number',)
+    date_hierarchy = 'created_at'
+    list_display = ('order_number', 'created_at', 'price', 'status', 'payment_uuid', 'is_test')
+    list_filter = ('is_test', 'status', 'created_at')
+    search_fields = ('order_number', )
