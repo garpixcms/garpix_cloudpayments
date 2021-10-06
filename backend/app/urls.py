f4455989e475701e2fca144f8c0d064d0702febc
marychev <marychevmihail3737@gmail.com>
2021-10-06 15:55:21 +0300

    Fixed app urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,8 @@
-from garpixcms.urls import path, include, urlpatterns
+from garpixcms.urls import path, include, urlpatterns as _urlpatterns
 from .views import example_view
 
 
-urlpatterns += [
-    path('', example_view),
-    path('example', example_view),
+urlpatterns = [
+    path('example/', example_view),
     path('cloudpayments/', include('garpix_cloudpayments.urls')),
-]
+] + _urlpatterns
